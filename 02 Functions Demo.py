dfd1e327aae21fc0274723de7f39c991343a3b9d
stefan-scott <scottst@spsd.sk.ca>
2019-04-03 09:23:49 -0600

    Create 02 Functions Demo.py

---
+++ 02 Functions Demo.py
@@ -0,0 +1,13 @@
+def fortune_one():
+    print("Don't step on the cracks! ")
+
+def fortune_two():
+    print("You will meet a new friend today! ")
+    
+def fortune_three():
+    print("Your lucky number is 3021 ")
+
+print("yooooou will have threeee fortunes...")
+fortune_one()
+fortune_two()
+fortune_three()
