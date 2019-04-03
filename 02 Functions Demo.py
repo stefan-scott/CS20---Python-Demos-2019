a6bcab35496912ddbc6274ad4a5b672bd673188c
stefan-scott <scottst@spsd.sk.ca>
2019-04-03 09:56:23 -0600

    Update 02 Functions Demo.py

--- 02 Functions Demo.py
+++ 02 Functions Demo.py
@@ -1,13 +1,43 @@
-def fortune_one():
-    print("Don't step on the cracks! ")
+##def fortune_one():
+##    print("Don't step on the cracks! ")
+##
+##def fortune_two():
+##    print("You will meet a new friend today! ")
+##    
+##def fortune_three():
+##    print("Your lucky number is 3021 ")
+##
+##print("yooooou will have threeee fortunes...")
+##fortune_one()
+##fortune_two()
+##fortune_three()
+##
 
-def fortune_two():
-    print("You will meet a new friend today! ")
-    
-def fortune_three():
-    print("Your lucky number is 3021 ")
+#Demo Two - Greeting
 
-print("yooooou will have threeee fortunes...")
-fortune_one()
-fortune_two()
-fortune_three()
+##def greeting():
+##    print("Nice to meet you", name)
+##
+##name = input("Enter a name: ")
+##greeting()
+##
+##name = input("Enter a name: ")
+##greeting()
+##
+##name = input("Enter a name: ")
+##greeting()
+
+num1 = int( input("Enter a number: ")  )
+num2 = int( input("Enter a number: ")  )
+sum = 0
+
+def add_numbers():
+    global sum
+    sum = num1 + num2
+
+add_numbers()
+print(sum)
+
+
+
+
